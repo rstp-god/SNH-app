bf377e2f39e5263d5e2d51457f7bd1d034f33740
turuguldinoff <turuguldinov.i@yandex.ru>
2021-04-25 21:51:04 +0300

    mini fix

--- sbh-fe/src/modules/Header/Header.js
+++ sbh-fe/src/modules/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import burgerImg from "./img/Burger.png"
-import hburgerImg from "./img/hBurger.png"
+import burgerImg from "./../../img/Burger.png"
+import hburgerImg from "./../../img/hBurger.png"
 
 const Container = styled.div`
   margin: 0 auto;
