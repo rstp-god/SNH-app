4af51a7ab50c693c97525dc9eab7e692ff28fd08
turuguldinoff <turuguldinov.i@yandex.ru>
2021-04-27 17:13:58 +0300

    adaptif-heder-noPadding

--- sbh-fe/src/modules/Header/Header.js
+++ sbh-fe/src/modules/Header/Header.js
@@ -1,55 +1,50 @@
 import React from "react";
 import styled from "styled-components";
-import burgerImg from "./../../img/Burger.png"
-import hburgerImg from "./../../img/hBurger.png"
+//import burgerImg from "./../../img/Burger.png"
+//import hburgerImg from "./../../img/hBurger.png"
 import logoImg from "./../../img/logo.png"
 
 
 const Container = styled.div`
   margin: 0 auto;
   height: 100px;
-  min-width: 260px;
-  max-width: 1200px;
+  /* min-width: 260px; */
+  /* max-width: 1200px; */
   box-sizing: border-box;
 `
 const Head = styled.header`
   background-color: #FFF;
-  width: 100%;
+  /* width: 100%; */
   height: 100px;
   float: left;
 `
 
 const Head2 = styled(Container)`
-  margin-top: 30px;
+  margin: 30px 0;
   height: 40px;
-  width: 85vw;
-  max-width: 1090px;
-  min-width: 220px;
-  background-color: #FFF;;
+  width: calc(240px + (1100 - 240) * ((100vw - 280px) / (1200 - 280)));
+  background-color: #FFF;
   float: left;
-`
-const H1 = styled.h1`
-  height: 40px;
-  margin:0;
-  width: 100px;
-  float: left;
-  color: #DFD4D4;
-  font-size: 25px;
-`
+  `
+
+// const H1 = styled.h1`
+  // height: 40px;
+  // margin:0;
+  // width: 100px;
+  // float: left;
+  // color: #DFD4D4;
+  // font-size: 25px;
+// `
 
 const Logo = styled.div`
   font-size: 28px;
   color: #171515;
-  width: 10vw;
-  height: 10vw;
-  max-width: 80px;
-  min-width: 40px;
-  max-height: 80px;
-  min-height: 40px;
+  width: calc(40px + (80 - 40) * ((100vw - 280px) / (1200 - 280)));
+  height: calc(40px + (80 - 40) * ((100vw - 280px) / (1200 - 280)));
   border: 2px;
   border-radius: 50px;
   background-image: url(${logoImg});
-  margin: 10px 0 0 0;
+  margin: calc(30px + (10 - 30) * ((100vw - 280px) / (1200 - 280))) 0;
   float: left;
 `
 
@@ -66,26 +61,23 @@ const Burger = styled.img`
 `
 
 const Menu = styled.menu`
-  width: 80%;
+  width: calc(240px + (1060 - 240) * ((100vw - 280px) / (1200 - 280)));
   height: 30px;
   float: right;
   margin: 0;
   padding: 0px 0;
-  min-width: 160px;
-  max-width: 884px;
   font-size: 25px;
 `
 
 const Btn = styled.a`
   padding: 5px 0;
-  width: 22%; 
-  min-width: 50px; 
+  width: calc(50px + (240 - 50) * ((100vw - 280px) / (1200 - 280))); 
   height: 30px;
   text-align: center;
   font-size: 12px;
   display: inline-block;
-  margin: 0 1%;
-  border: 2px solid #DFD4D4;
+  /* margin: 0 1%; */
+  /* border: 2px solid #DFD4D4; */
   box-sizing: border-box;
   border-radius: 10px;
   color: #171515;
