c48fae77c0ea1bc5554395de5377573ccde7970b
rstp <39411889+azirb@users.noreply.github.com>
2021-04-28 22:12:38 +0300

    add video and video sizing for inpect block

--- sbh-fe/src/modules/Formula Inspect/Inspect.js
+++ sbh-fe/src/modules/Formula Inspect/Inspect.js
@@ -29,6 +29,7 @@ const CalculatorContaner= styled.div`
     display: flex; 
     flex-direction:column;
     width: 100%; 
+    padding: 10px; 
     box-sizing: border-box; 
     margin: 2% auto; 
     border: 1px solid black; 
@@ -57,14 +58,46 @@ const Description=styled.div`
     width: 100%; 
     font-size:15px;
     box-sizing: border-box; 
-    margin: 0 auto; 
+    padding: 10px;
+    margin: 2% auto; 
     border: 1px solid black;
 `
 
-const test = "$$\\frac{\\int_{-\\infty}^{+\\infty}ydx}{\\sum \\alpha \\beta +\\int x^2dx}$$"; 
+const VideoContainer= styled.div`
+    width: 100%; 
+    box-sizing: border-box; 
+    margin: 2% auto; 
+    padding: 10px;
+    border: 1px solid black;
+`
+
+const Video =styled.iframe`
+    width: 100%; 
+    margin: 0 auto; 
+    height: 600px;
+
+@media (max-width: 1200px) {
+    height: 500px;
+}
+
+@media (max-width: 500px) {
+    height: 350px;
+}
+@media (max-width: 350px) {
+    height: 280px;
+}
+`
+
+const LaTeXFormula = "$$\\frac{\\int_{-\\infty}^{+\\infty}ydx}{\\sum \\alpha \\beta +\\int x^2dx}$$"; 
+const VideoUrl = "https://www.youtube.com/embed/u_pnia4Xhlw";
 
 
 export default class Inspect extends Component {
+
+    constructor(props){
+        super(props); 
+        
+    }
 
     render() {
         return ( 
@@ -74,7 +107,7 @@ export default class Inspect extends Component {
              </NavContainer>
              <FormulaContainer>
                  <MathJax.Provider>
-                    <MathJax.Formula formula={test}/> 
+                    <MathJax.Formula formula={LaTeXFormula}/> 
                  </MathJax.Provider>
              </FormulaContainer>
              <CalculatorContaner>
@@ -84,6 +117,7 @@ export default class Inspect extends Component {
                 <InputCalc/>
                 <InputCalc/>
                 </InputContainer>
+                <Answer/>
              </CalculatorContaner>
              <Description>
                     <h1>Description</h1>
@@ -91,6 +125,10 @@ export default class Inspect extends Component {
              <br/>  
              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan metus in mollis euismod. Nunc finibus tincidunt odio, et porta dolor dapibus sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam vitae ante ultrices, placerat purus id, lacinia nulla. Morbi mattis ex non scelerisque pulvinar. In hac habitasse platea dictumst. Nullam sagittis elit ligula, nec suscipit odio sagittis et.
              </Description>
+             <VideoContainer> 
+                <h1>Checkout this!</h1>
+                <Video src={VideoUrl}  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>
+             </VideoContainer>
         </Container>
         ) 
     }
