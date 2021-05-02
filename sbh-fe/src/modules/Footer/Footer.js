49487d76d980aad08e71ab313c3cb79ac1d3a3e2
turuguldinoff <turuguldinov.i@yandex.ru>
2021-05-02 23:26:08 +0300

    Footer-up

---
+++ sbh-fe/src/modules/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 auto;
+  height: 100px;
+  box-sizing: border-box;
+`
+
+class Footer extends React.Component{
+render(){
+
+  return (
+      
+  )
+}}
+
+export default Footer
