795e5af7c45bc22bf4d2a71cb1031fb2dcae2704
rstp <39411889+azirb@users.noreply.github.com>
2021-05-19 21:50:16 +0300

    add actions

--- sbh-fe/src/reducer.js
+++ sbh-fe/src/reducer.js
@@ -14,32 +14,43 @@ let InitialState = {
 if (cookie.getCookiebyName('lastformula') !== undefined || 
     cookie.getCookiebyName('lastformula') !=='') {
     InitialState = { 
+        ...InitialState,
         formula : cookie.getCookiebyName('lastformula'), 
-        formulas : [], 
-        block: null, 
-        loading : true, 
-        args: undefined,
-        answer : null 
     }
 }
 
 if (cookie.getCookiebyName('lastblock') !== undefined || 
 cookie.getCookiebyName('lastblock') !=='') {
 InitialState = { 
-    formula : InitialState.formula, 
-    formulas : [], 
-    block: cookie.getCookiebyName('lastblock'), 
-    loading : true, 
-    args: undefined,
-    answer : null 
+    ...InitialState,
+    block: cookie.getCookiebyName('lastblock'),
 }
 }
 
 const reducer = (state = InitialState, action) => { 
     switch(action) {
-        case 'INIT': 
-        {
-            return state = 'init'; 
+        case 'FORMULA_LOADED': {
+             return {
+                 ...state, 
+                 formula : action.payload.formula,
+                 args : action.payload.args, 
+                 loading : false
+             }
+        }
+        case 'FORMULA_LIST_LOADED' : {
+            return {
+                ...state, 
+                formulas : action.payload.List, 
+                block : action.payload.block,
+                loading : false
+            }
+        }
+        case 'ANSWER_LOADED' : { 
+            return { 
+                ...state, 
+                answer : action.payload.answer, 
+                loading: false
+            }
         }
     }
 }
