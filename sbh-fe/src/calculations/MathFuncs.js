e046121e69995d7fe1825d28691927a3a17fd905
rstp <39411889+azirb@users.noreply.github.com>
2021-05-23 23:28:24 +0300

    rework hashing of formules, action fix

--- sbh-fe/src/calculations/MathFuncs.js
+++ sbh-fe/src/calculations/MathFuncs.js
@@ -1,19 +1,22 @@
-export default [
-    {
+const MathFuncArray = []; 
+
+
+MathFuncArray['formula1']= { 
         id: 1, 
         args: 3, 
         func: sum
-    }, 
-    { 
-        id: 2 , 
-        args : 3, 
-        func : evalsquare
-    }
-]
+};
 
 function sum(a,b,c) {
     return a+b+c; 
 }
+
+
+MathFuncArray['formula2']={ 
+    id: 2 , 
+    args : 3, 
+    func : evalsquare
+}; 
 
 function evalsquare(a,b,c) {
     const x1 = a+b; 
@@ -21,4 +24,4 @@ function evalsquare(a,b,c) {
     return [x1 ,x2]; 
 }
 
-//ZARABOTAI
+export default MathFuncArray; 
